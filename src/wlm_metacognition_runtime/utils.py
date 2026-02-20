f325175478914cd04dcc426c6dad61b271d95e37
Wujie Gavin Gu <gavin.gu2255@gmail.com>
2026-02-20 11:41:52 +1100

    Create utils.py

---
+++ src/wlm_metacognition_runtime/utils.py
@@ -0,0 +1,11 @@
+# utils.py
+```python
+"""
+Utility helpers for WLM‑Metacognition‑Runtime.
+"""
+
+import json
+
+def pretty_print(obj):
+    print(json.dumps(obj, indent=2, ensure_ascii=False))
+```
